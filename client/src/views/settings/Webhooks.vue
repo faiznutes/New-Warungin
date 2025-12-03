b6e77f1015738b60db0f84a05e9d9c689190f94a
faiznutes <faiznute07@gmail.com>
2025-12-04 02:48:22 +0700

    fix: add w-4 h-4 to remaining checkbox in Webhooks.vue

--- client/src/views/settings/Webhooks.vue
+++ client/src/views/settings/Webhooks.vue
@@ -226,7 +226,7 @@
                 <input
                   v-model="webhookForm.isActive"
                   type="checkbox"
-                  class="rounded border-gray-300 text-primary-600 focus:ring-primary-500"
+                  class="w-4 h-4 rounded border-gray-300 text-primary-600 focus:ring-primary-500"
                 />
                 <span class="text-sm text-gray-700">Aktif</span>
               </label>
